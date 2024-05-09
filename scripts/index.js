940d3e548c5d107b9701e1b84f9f41b01a2cc435
WilliamFerns1 <willfv2@gmail.com>
2024-05-09 09:43:41 +0200

    feat(styles): added in all local variables from figma to stylesheet

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const menuButton = document.getElementById("menu-btn");
+const navItems = document.getElementById("nav-list");
+
+let isMenuActive = false;
+
+let rotation = 0;
+const toggleNavList = () => {
+  isMenuActive = !isMenuActive; toggleMenu(isMenuActive);
+  menuButton.style.rotate = `${rotation}deg`;
+}
+
+const isMobile = () => {
+  return window.innerWidth <= 512;
+}
+
+const toggleMenu = (isMenuActive) => {
+  if (isMobile()) {
+    if (isMenuActive) {
+      navItems.classList = "slide-from-right";
+      navItems.style.display = "flex";
+      menuButton.classList = "fa-solid fa-xmark";
+      rotation += 360;
+    } else {
+      navItems.classList = "slide-from-left";
+      menuButton.classList = "fa-solid fa-bars";
+      rotation -= 360;
+    }
+  }
+}
+
+menuButton.addEventListener("click", toggleNavList);
