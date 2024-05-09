382756cae209f67d25d2339c5ec075769beae79c
WilliamFerns1 <willfv2@gmail.com>
2024-05-09 09:08:24 +0200

    feat: empty project

--- src/theme/themeConfig.ts
+++
@@ -1,8 +0,0 @@
-import { ThemeConfig } from "@chakra-ui/react"
-
-const themeConfig: ThemeConfig = {
-  initialColorMode: 'dark', // 'dark' | 'light'
-  useSystemColorMode: false,
-}
-
-export default themeConfig; 
