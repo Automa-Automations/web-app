2761cf1479c1f8f216d403278b8d0583c02f897f
WilliamFerns1 <willfv2@gmail.com>
2024-05-09 18:31:44 +0200

    feat(badge): added badge text styles

--- scripts/setGlobalStyles.js
+++ scripts/setGlobalStyles.js
@@ -1,9 +1,17 @@
 // THIS FUNCTION IS USED TO SET CLASS DEFAULT STYLES TO ELEMENT, FOR EXAMPLE TEXTSTYLES.
 
+const addClass = (selector, classToAdd) => {
+  const elements = document.querySelectorAll(selector);
+  if (elements) {
+    elements.forEach((element) => element.classList.add(classToAdd));
+  }
+};
+
 const setGlobalStyles = () => {
   // Set buttons default text size.
-  const allButtons = document.querySelectorAll("button");
-  allButtons.forEach((button) => button.classList.add("headlinesm"));
+  addClass("button", "headlinesm");
+  // Set badge text styles
+  addClass(".badge", "badge-text");
 }
 
 setGlobalStyles();
