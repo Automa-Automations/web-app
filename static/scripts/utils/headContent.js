682abe26d0955a14108fe37cde207bec1e4c06ce
WilliamFerns1 <willfv2@gmail.com>
2024-05-17 12:22:53 +0200

    refactor(HeadContent): made class, with static methods
    HeadContent class is a class with the addComponentStyleSheet and addHeadContent functions as static methods. It replaces those files. The class is used for anything to do with creating/modifying the head content.
    
    BREAKING CHANGE: The codebase still uses the old methods, not the class yet. Because of that, it will give errors.

---
+++ static/scripts/utils/headContent.js
@@ -0,0 +1,39 @@
+class HeadContent {
+  static addComponentStyleSheetToHead(componentName) {
+    const head = document.querySelector("head");
+
+    const link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", `/static/css/components/${componentName}.css`);
+
+    head.appendChild(link);
+  };
+
+  static addHeadContent({
+    route,
+    title = "Automa Automations",
+    description = "Automa is a project that focuses on making AI & AI automation available to the general public at a low-low cost of 10$ for the base hosted plan, or 0$ for self hosting all of our infrastructure.",
+    iconPath = "/static/assets/brand/automa-icon.svg",
+    customHTML = "", // Pass this in as an argument when you want to add in some custom tags into the head.
+    textStyles = false,
+  }) {
+    const headContent = `
+      <title>${title}</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="${description}" />
+      <link href="/static/css/theme/index.css" rel="stylesheet" />
+      <link href="/static/css/routes/${route}.css" rel="stylesheet" />
+      <link rel="icon" type="x-icon/image" href="${iconPath}" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+      ${textStyles ? '<link href="/static/css/theme/textStyles.css" rel="stylesheet">' : ""}
+      ${customHTML}
+    `;
+    const headElement = document.querySelector("head");
+    headElement.innerHTML = headContent;
+  };
+}
+
+export default HeadContent;
